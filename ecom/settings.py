931d3dcbc38a92d2be5c7b84578368a69325b96a
Atonu Shaha <atonushaha@Atonus-MacBook-Air.local>
2024-11-01 01:23:53 +0600

    potential solve

--- ecom/settings.py
+++ ecom/settings.py
@@ -61,8 +61,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'ecom.middleware.DisableLoginRedirectForSwaggerMiddleware',
-
+   
 ]
 
 
